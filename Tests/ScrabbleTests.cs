8e08315ba86ec3194defa78fd08357304ee9f09a
YcleptInsan <nickmwise@gmail.com>
2017-06-01 10:09:09 -0700

    Add basic layout for testing environment.

---
+++ Tests/ScrabbleTests.cs
@@ -0,0 +1,20 @@
+using Xunit
+
+namespace Scrabble.Objects
+{
+	public class ScrabbleTests
+	{
+
+		[FACT]
+		public void Is_Letter_Equal_To_K_True()
+		{
+			// Assign
+			Scrabble testScrabble = new Scrabble();
+
+			// Act
+
+
+			//Assert
+		}
+	}
+}
